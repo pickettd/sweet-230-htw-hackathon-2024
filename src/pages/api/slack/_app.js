e041965d210e25030d1e1154317d7661714e9cd6
Ben Ward <ben@mauilabs.com>
2024-09-15 18:44:22 -1000

    WIP: Gets the bot token and stores on FS. PrismIntallationStore not working

--- src/pages/api/slack/_app.js
+++ src/pages/api/slack/_app.js
@@ -1,13 +1,45 @@
 import { App, LogLevel, FileInstallationStore } from "@slack/bolt";
 import { FileStateStore } from "@slack/oauth";
 import { AppRunner } from "@seratch_/bolt-http-runner";
+// import { PrismaClient } from '@prisma/client';
+// import { PrismaInstallationStore } from '@seratch_/bolt-prisma';
+//
+// const prismaClient = new PrismaClient({
+//   log: [
+//     {
+//       emit: 'stdout',
+//       level: 'query',
+//     },
+//   ],
+// });
+//
+// const installationStore = new PrismaInstallationStore({
+//   // The name `slackAppInstallation` can be different
+//   // if you use a different name in your Prisma schema
+//   prismaTable: prismaClient.slackAppInstallation,
+//   clientId: process.env.SLACK_CLIENT_ID,
+// });
+//
+// // Use PrismaInstallationStore
+// export const appRunner = new AppRunner({
+//   logLevel: LogLevel.DEBUG,
+//   // token: process.env.SLACK_BOT_TOKEN,
+//   signingSecret: process.env.SLACK_SIGNING_SECRET,
+//   clientId: process.env.SLACK_CLIENT_ID,
+//   clientSecret: process.env.SLACK_CLIENT_SECRET,
+//   stateSecret: process.env.SLACK_STATE_SECRET,
+//   scopes: ["commands", "chat:write", "app_mentions:read"],
+//   installationStore
+// });
 
+// Use FileInstallationStore
 export const appRunner = new AppRunner({
   logLevel: LogLevel.DEBUG,
   // token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   clientId: process.env.SLACK_CLIENT_ID,
   clientSecret: process.env.SLACK_CLIENT_SECRET,
+  stateSecret: process.env.SLACK_STATE_SECRET,
   scopes: ["commands", "chat:write", "app_mentions:read"],
   installationStore: new FileInstallationStore(),
   installerOptions: {
